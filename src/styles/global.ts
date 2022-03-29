434700d7459469554b3f5295bb4db885b9b892ea
Michel Wene <michelwene@hotmail.com>
2022-03-29 16:01:46 -0300

    style: :lipstick: add globalstyles

---
+++ src/styles/global.ts
@@ -0,0 +1,96 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    outline: 0;
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    -webkit-font-smoothing: antialiased;
+
+    &.lockScroll {
+      max-height: 100vh;
+      overflow: hidden;
+    }
+  }
+
+  body, input, button, textarea, select {
+    font: 400 1rem "Heebo", sans-serif;
+  }
+
+  button {
+    cursor: pointer;
+  }
+
+  a {
+    text-decoration: none;
+  }
+
+  li {
+    list-style-type: none;
+  }
+
+  #__next {
+    min-height: 100vh;
+  }
+
+
+  @media(max-width: 1080px) {
+    html {
+      font-size: 93.75%;
+    }
+  }
+  @media(max-width: 720px) {
+    html {
+      font-size: 87.50%;
+    }
+  }
+
+  :root {
+   //colors
+    --main:#2AD2C9;
+    --secondary:#1B1D36;
+    --bg:#F6F8F9;
+    --white: #ffffff;
+
+    --pink:#ED2E7E;
+    --rose:#F59090;
+    --violet:#9290F5;
+    --green:#00BA88;
+
+    --yellow:#F4B740;
+    --purple:#4164E3;
+
+  }
+  input[type='date']::-webkit-inner-spin-button,
+  input[type='date']::-webkit-calendar-picker-indicator {
+    position: relative;
+    opacity: 0;
+    z-index: 5;
+  }
+
+  &::-webkit-scrollbar {
+    width: 4px;
+    height: 4px;
+    border-radius: 0;
+  }
+  &::-webkit-scrollbar-thumb {
+    background: var(--gray-300);
+    border-radius: 0;
+  }
+  &::-webkit-scrollbar-track {
+    border-radius: 0;
+    background: rgba(0,0,0,0.05);
+  }
+
+  @media (max-width: 425px) {
+    ::-webkit-scrollbar,
+    ::-webkit-scrollbar-thumb,
+    ::-webkit-scrollbar-track {
+      display: none;
+    }
+  }
+`;
